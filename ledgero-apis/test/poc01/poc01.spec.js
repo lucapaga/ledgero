1103255ae79ea31557ef2507c7c5007fa80f1389
Luca Paganelli <luca.paga@gmail.com>
2016-12-31 12:42:39 +0100

    preparing unit tests

---
+++ ledgero-apis/test/poc01/poc01.spec.js
@@ -0,0 +1,19 @@
+describe('poc01', function () {
+
+  //beforeEach(module('calculatorApp'));
+
+  //var $controller;
+
+  /*
+  beforeEach(inject(function(_$controller_){
+    $controller = _$controller_;
+  }));
+  */
+
+  describe('poc01-01', function () {
+		it('1 + 1 should equal 2', function () {
+			expect(2).toBe(3);
+		});
+	});
+
+});
