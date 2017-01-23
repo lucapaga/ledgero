eaa6a85b3c8495e4b872320530d19e1fa70c0978
Luca Paganelli <luca.paga@gmail.com>
2017-01-23 23:03:22 +0100

    refactoring / 2

---
+++ ledgero-web/src/routes/AccountList/actions/actionGenerators.js
@@ -0,0 +1,26 @@
+import { LOAD_MY_ACCOUNTS, LOADING_MY_ACCOUNTS } from './actionTypes'
+
+export const createLoadingAction = () => {
+  console.log("Dispatching event '",LOADING_MY_ACCOUNTS,"' ...");
+  return { type: LOADING_MY_ACCOUNTS };
+}
+
+export const createMockedAccountListAction = () => {
+  return {
+        type        : LOAD_MY_ACCOUNTS,
+        accountList : [{
+              accountId: "28393298",
+              name: "Root",
+              description: "Your root account. You can't change it"
+            }, {
+              accountId: "65664252",
+              name: "Pippo",
+              description: "My Custom"
+            }, {
+              accountId: "3787887328",
+              name: "Pluto",
+              description: "My Caztom"
+          }],
+        nrOfResults : 3
+      };
+}
